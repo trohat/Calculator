0459e0427add45a775e4ffbe83ff8e75cd7813a6
Trohat <petr.sed@centrum.cz>
2020-08-20 11:43:21 +0200

    Rewrited with Linq to XML API

--- SOAP Calculator/Program.cs
+++ SOAP Calculator/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Net;
 using System.Xml;
+using System.Xml.Linq;
 
 namespace SOAP_Calculator
 {
@@ -22,8 +23,10 @@ namespace SOAP_Calculator
             //Calling CreateSOAPWebRequest method  
             HttpWebRequest request = CreateSOAPWebRequest();
 
-            XmlDocument SOAPReqBody = new XmlDocument();
+            /*
             //SOAP Body Request  
+            XmlDocument SOAPReqBody = new XmlDocument();
+
             SOAPReqBody.LoadXml(@"<?xml version=""1.0"" encoding=""utf-8""?>  
             <soap:Envelope xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance""
                            xmlns:xsd=""http://www.w3.org/2001/XMLSchema""
@@ -35,10 +38,43 @@ namespace SOAP_Calculator
                 </Add>  
               </soap:Body>  
             </soap:Envelope>");
-            Console.WriteLine("OuterXml:" + SOAPReqBody.OuterXml);
-            Console.WriteLine("InnerText:" + SOAPReqBody.InnerText); 
-            Console.WriteLine("InnerXml:" + SOAPReqBody.InnerXml); 
+            */
+            
+            string soapUrl = "http://schemas.xmlsoap.org/soap/envelope/";
+            XNamespace soap = soapUrl;
+
+            string xsdUrl = "http://www.w3.org/2001/XMLSchema";
+            XNamespace xsd = xsdUrl;
+
+            string xsiUrl = "http://www.w3.org/2001/XMLSchema-instance";
+            XNamespace xsi = xsiUrl;
+
+            string tempUriString = "http://tempuri.org/";
+            XNamespace tempUri = tempUriString;
+
+
+            XDocument SOAPReqBody = new XDocument(
+                //new XDeclaration("1.0", "utf-8", "yes"),
+                new XElement(soap + "Envelope",
+                    new XAttribute(XNamespace.Xmlns + "soap", soapUrl),
+                    new XAttribute(XNamespace.Xmlns + "xsd", xsdUrl),
+                    new XAttribute(XNamespace.Xmlns + "xsi", xsiUrl),
+                    new XElement(soap + "Body",
+                        new XElement(tempUri + "Add",
+                            new XElement(tempUri + "intA", a),
+                            new XElement(tempUri + "intB", b)
+                        )
+                    )
+                )
+            );
+
+            SOAPReqBody.Save("Root.xml");
+
+            Console.WriteLine(File.ReadAllText("Root.xml"));
+
+            Console.WriteLine("ToString:" + SOAPReqBody.ToString());
             Console.WriteLine("Waiting for resp!");
+            //Console.ReadLine();
 
 
             using (Stream stream = request.GetRequestStream())
